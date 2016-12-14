233d5520f218fd2a83eb74b0658a2d5fbbbd4a01
pfaff <johanv@tru-cape.co.za>
2016-12-14 14:52:40 +0200

    Signed-off-by: pfaff <johanv@tru-cape.co.za>

--- fruit.fly.file.utility/src/fruit/fly/file/utility/TestRead.java
+++ fruit.fly.file.utility/src/fruit/fly/file/utility/TestRead.java
@@ -3,11 +3,16 @@ package fruit.fly.file.utility;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 
 public class TestRead {
+
+	private List<File> loadList = new ArrayList<File>();
+	private List<File> keepList = new ArrayList<File>();
 
 	public TestRead() throws Exception {
 
@@ -35,19 +40,11 @@ public class TestRead {
 	}
 
 	private void readLine(String line,String file) throws Exception {
-		
-		
+
 		System.out.println(":) " + file + " :: " + line.substring(2,8));;
-		
-		
-		
+
 	}
 
-	
-	
-	
-	
-	
 	private File pick() throws Exception {
 		JFileChooser fc = new JFileChooser();
 		fc.setCurrentDirectory(new File("C:/load"));
@@ -58,6 +55,26 @@ public class TestRead {
         return null;
 	}
 
+	public List<File> getLoadList() {
+		return loadList;
+	}
+
+	public void setLoadList(List<File> loadList) {
+		this.loadList = loadList;
+	}
+
+	public List<File> getKeepList() {
+		return keepList;
+	}
+
+	public void setKeepList(List<File> keepList) {
+		this.keepList = keepList;
+	}
+
+
+	
+	
+	
 	public static void main(String[] args) {
 		try {
 			new TestRead();
